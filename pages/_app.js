58a731d7f91f83d5cb13ebb2368e401e8188ac9f
Keys HD <keys208@hotmail.com>
2021-07-21 12:30:13 -0300

    Bug fix & Website icon added

--- pages/_app.js
+++ pages/_app.js
@@ -8,10 +8,25 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Alura Challenge</title>
+        <title>Aluradev - Alura Challenges</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto&display=swap" rel="stylesheet"/>
+        <link rel="icon" type="image/png" href="./favicon.ico" />
+        <meta name="title" content="Aluradev - Alura Challenges"/>
+        <meta name="description" content="Editor de códigos com snippets com opção de salvar e exportar para extensão png, svg, jpeg."/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://aluradev.vercel.app/"/>
+        <meta property="og:title" content="Aluradev - Alura Challenges"/>
+        <meta property="og:description" content="Editor de códigos com snippets com opção de salvar e exportar para extensão png, svg, jpeg."/>
+        <meta property="og:image" content=""/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://aluradev.vercel.app/"/>
+        <meta property="twitter:title" content="Aluradev - Alura Challenges"/>
+        <meta property="twitter:description" content="Editor de códigos com snippets com opção de salvar e exportar para extensão png, svg, jpeg."/>
+        <meta property="twitter:image" content=""/>
       </Head>
       <Navbar>
         <ThemeWrapper>
